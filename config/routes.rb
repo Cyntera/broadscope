da4c3c032370e00ea164511fe9221d5be93488c3
Cyntera <ccamper365@gmail.com>
2016-10-28 13:58:47 -0700

    Route new pages to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'pages/contact'
+
+  get 'pages/homebodies'
+
+  get 'pages/events'
+
+  get 'pages/news'
+
   root 'pages#home'
+
+  get '/news' => 'pages#news'
+
+  get '/events' => 'pages#events'
+
+  get '/homebodies' => 'pages#homebodies'
+
+  get '/forum' => 'pages#forum'
+
+  get '/contact' => 'pages#contact'
 end
